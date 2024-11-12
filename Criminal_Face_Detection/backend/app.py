e93eaaad2a0f30bf7c9362061636cd7e60f97b8c
yuvraj <73472747+m-yuvraj@users.noreply.github.com>
2024-11-12 13:38:44 +0530

    Update app.py
    Main Flask Applications

--- Criminal_Face_Detection/backend/app.py
+++ Criminal_Face_Detection/backend/app.py
@@ -1,20 +1,26 @@
 from flask import Flask, request, jsonify
-from main import detect_from_image  # Import function from main.py
 from flask_cors import CORS
+from detect_module import detect_from_image
+from upload_module import upload_criminal_image
 
 app = Flask(__name__)
-CORS(app)  # Allow CORS for communication with frontend
+CORS(app)
 
 @app.route("/detect", methods=["POST"])
 def detect():
     if "file" not in request.files:
         return jsonify({"error": "No file uploaded"}), 400
     file = request.files["file"]
-
-    # Run detection from main.py
     result = detect_from_image(file)
+    return jsonify(result)
+
+@app.route("/upload_criminal", methods=["POST"])
+def upload_criminal():
+    if "file" not in request.files:
+        return jsonify({"error": "No file uploaded"}), 400
+    file = request.files["file"]
+    result = upload_criminal_image(file)
     return jsonify(result)
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
